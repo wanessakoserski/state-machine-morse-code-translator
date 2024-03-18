a93ea6d3269cce6d142a8caa44d67ca691c4a3d6
Wanessa Koserski <wkoserski@gmail.com>
2024-03-18 14:34:19 -0300

    feat: tutorial in start

--- main.rb
+++ main.rb
@@ -2,5 +2,14 @@ require_relative 'from_morse_code'
 
 translate_from_morse_code = FromMorseCode.new
 
-puts 'Digite a sequência: '
+puts "\nTradutor de código morse"
+puts "~ esse tradutor utilizar a transição de estados"
+puts "~ insira . ou -"
+puts "~ importante ser um de cada vez, ou seja, insira ./- e de ENTER para transicionar o estado"
+puts "~ quando terminar a sequência ./- da letra tecle ENTER para processar o fim da letra"
+puts "~ caso também seja fim da palavra também, decle novamente ENTER para processar o fim da palavra e inserir espaço"
+puts "~ caso não haja mais nenhuma palavra, tecle novamente ENTER para finalizar a tradução"
+puts "~ 1º ENTER: fim de letra, 2º ENTER: fim de palavra, 3º ENTER: fim de tradução"
+
+puts "\n\nDigite a sequência: "
 translate_from_morse_code.start
