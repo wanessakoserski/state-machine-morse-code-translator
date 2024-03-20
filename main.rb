e73e37517b4e303f8f861ac08e0ed4eacda6a3c3
Wanessa Koserski <wkoserski@gmail.com>
2024-03-19 23:50:47 -0300

    style: correct grammar error

--- main.rb
+++ main.rb
@@ -5,9 +5,9 @@ translate_from_morse_code = FromMorseCode.new
 puts "\nTradutor de código morse"
 puts "~ esse tradutor utilizar a transição de estados"
 puts "~ insira . ou -"
-puts "~ importante ser um de cada vez, ou seja, insira ./- e de ENTER para transicionar o estado"
-puts "~ quando terminar a sequência ./- da letra tecle ENTER para processar o fim da letra"
-puts "~ caso também seja fim da palavra também, decle novamente ENTER para processar o fim da palavra e inserir espaço"
+puts "~ importante ser um de cada vez, ou seja, insira . / - e de ENTER para transicionar o estado"
+puts "~ quando terminar a sequência . / - da letra tecle ENTER para processar o fim da letra"
+puts "~ caso também seja fim da palavra, tecle novamente ENTER para processar o fim da palavra e inserir espaço"
 puts "~ caso não haja mais nenhuma palavra, tecle novamente ENTER para finalizar a tradução"
 puts "~ 1º ENTER: fim de letra, 2º ENTER: fim de palavra, 3º ENTER: fim de tradução"
 
